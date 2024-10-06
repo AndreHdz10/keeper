a354afab4bf9ec7070856ec92422c76316af2249
Andre <aandreehdz@gmail.com>
2024-10-05 22:15:47 -0600

    Final Stage - Notes Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Andre Hernandez CSE 310ⓒ {year}</p>
     </footer>
   );
 }
